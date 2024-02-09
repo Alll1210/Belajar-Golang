63420ae232759563d72c60742f3b665d3080525f
Alll1210 <izunauciha011@gmail.com>
2024-02-09 10:45:13 +0700

    Constant

---
+++ TEST/constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Ali Sya'bana"
+		lastName         = "Syukurillah"
+		nim              = 210111008
+	)
+
+	fmt.Println("Nama Lengkap =", firstName, lastName, "\nNIM =", nim)
+
+}
