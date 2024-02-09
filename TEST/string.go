e1944a17a87c766e1c5ac8a2f10b4dcd4ed2b09b
Alll1210 <izunauciha011@gmail.com>
2024-02-09 10:20:00 +0700

    String

---
+++ TEST/string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Test")
+	fmt.Println(len("Test"))
+	fmt.Println("Test"[2])
+	fmt.Println("Test 123")
+	fmt.Println(len("Test 123"))
+
+}
