0a76c3a60e15f0825ae70cd22e34513145de8794
Alll1210 <izunauciha011@gmail.com>
2024-02-09 11:44:40 +0700

    Operasi Matematika

---
+++ TEST/math-operations.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result = 15 - 10
+	fmt.Println(result)
+
+	var a = 10
+	var b = 3
+	var c = a * b
+	fmt.Println(c)
+
+	var i = 5
+	i += 5
+	fmt.Println(i)
+
+	i++
+	fmt.Println(i)
+
+}
